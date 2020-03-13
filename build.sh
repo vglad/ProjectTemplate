44a707b553d4aaee7d9bb5944bb599cf86927382
Volodymyr Gladun <vgladun@gmail.com>
2020-03-13 22:49:25 +0000

    build script changed

--- build.sh
+++ build.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
 cd ${PROJECT_DIR} || return 1
-BUILD_DIR=./build/release
-#BUILD_DIR=/opt/${PROJECT_DIR}/build/release
+BUILD_DIR=${PROJECT_DIR}/build/release
 
-if [ ! -d "$BUILD_DIR" ]; then
-  mkdir -p $BUILD_DIR
+if [ ! -d "${BUILD_DIR}" ]; then
+  mkdir -p ${BUILD_DIR}
 fi
 
-cd $BUILD_DIR || return 1
+cd ${BUILD_DIR} || return 1
 cmake -DCMAKE_BUILD_TYPE=Release ../..
 make
 
-$BUILD_DIR/tests/allTests
+${BUILD_DIR}/tests/allTests
