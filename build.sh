3ac84d83232c9c7177ac485bc8cfa380149eea25
Volodymyr Gladun <vgladun@gmail.com>
2020-03-13 22:38:20 +0000

    build script changed

--- build.sh
+++ build.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-BUILD_DIR=/opt/${PROJECT_DIR}/build/release
+cd ${PROJECT_DIR} || return 1
+BUILD_DIR=./build/release
+#BUILD_DIR=/opt/${PROJECT_DIR}/build/release
 
 if [ ! -d "$BUILD_DIR" ]; then
   mkdir -p $BUILD_DIR
 fi
 
-cd $BUILD_DIR || exit
+cd $BUILD_DIR || return 1
 cmake -DCMAKE_BUILD_TYPE=Release ../..
 make
 
