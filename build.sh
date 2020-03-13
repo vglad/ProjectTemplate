e154cf74146da55af2905c618695292c8dd99018
Volodymyr Gladun <vgladun@gmail.com>
2020-03-13 21:45:52 +0000

    build script changed

--- build.sh
+++ build.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-# Enable GCC 9.1.1
-source scl_source enable gcc-toolset-9
-
-RELEASE_BUILD_DIR=/root/build/release
+RELEASE_BUILD_DIR=/opt/build/release
 
 if [ ! -d "$RELEASE_BUILD_DIR" ]; then
   mkdir -p $RELEASE_BUILD_DIR
 fi
 
 cd $RELEASE_BUILD_DIR || exit
-cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release ../..
+cmake -DCMAKE_BUILD_TYPE=Release ../..
 make
 
 $RELEASE_BUILD_DIR/tests/allTests
