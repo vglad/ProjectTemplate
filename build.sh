fb5b561d3f1a84032075b037aba52b276b91924d
Volodymyr Gladun <vgladun@gmail.com>
2020-03-14 02:34:54 +0000

    matrix added to travis.yaml build script changed

--- build.sh
+++ build.sh
@@ -8,7 +8,16 @@ if [ ! -d "${BUILD_DIR}" ]; then
 fi
 
 cd ${BUILD_DIR} || return 1
-cmake -DCMAKE_BUILD_TYPE=Release ../..
+
+if [[ ${DOCKER} -eq 1 ]] && [[ "${COMPILER}" == "gcc" ]]; then
+  export CC='gcc'
+  export CXX='g++'
+elif [[ ${DOCKER} -eq 1 ]] && [[ "${COMPILER}" == "clang" ]]; then
+  export CC='clang'
+  export CXX='clang++'
+fi
+
+cmake -DCMAKE_BUILD_TYPE=Release ../.. -DCMAKE_C_COMPILER=${CC} -DCMAKE_CXX_COMPILER=${CXX} -DCMAKE_CXX_STANDARD=17 -DCMAKE_CXX_STANDARD_REQUIRED=ON -DCMAKE_CXX_EXTENSIONS=OFF
 make
 
 ${BUILD_DIR}/tests/allTests
