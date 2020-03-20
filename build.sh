12c71cdf460531840ad484e1ec22055c82f5c228
Volodymyr Gladun <vgladun@gmail.com>
2020-03-20 12:42:12 +0000

    build script configured for docker and non-docker environment made compiler options global for all targets

--- build.sh
+++ build.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+# Set project directory if building without Docker
+if [[ ${DOCKER} -eq 0 ]]; then
+  PROJECT_DIR=/opt/dev/ProjectTemplate
+fi
+
 cd ${PROJECT_DIR} || return 1
-BUILD_DIR=${PROJECT_DIR}/build/release
+BUILD_DIR=${PROJECT_DIR}/build/cmake-build-release
 
 if [ ! -d "${BUILD_DIR}" ]; then
   mkdir -p ${BUILD_DIR}
@@ -17,7 +22,14 @@ elif [[ ${DOCKER} -eq 1 ]] && [[ "${COMPILER}" == "clang" ]]; then
   export CXX='clang++'
 fi
 
-cmake -DCMAKE_BUILD_TYPE=Release ../.. -DCMAKE_C_COMPILER=${CC} -DCMAKE_CXX_COMPILER=${CXX} -DCMAKE_CXX_STANDARD=17 -DCMAKE_CXX_STANDARD_REQUIRED=ON -DCMAKE_CXX_EXTENSIONS=OFF
+# Set compiler if building without Docker
+if [[ ${DOCKER} -eq 0 ]]; then
+  export CC='gcc'
+  export CXX='g++'
+fi
+
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=${CXX} -DCMAKE_C_COMPILER=${CC} ../..
 make
 
+# Run tests
 ${BUILD_DIR}/tests/allTests
