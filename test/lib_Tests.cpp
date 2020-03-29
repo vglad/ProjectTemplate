020f29cf86edde4f79b5aa272e36e0adf478f8bc
Volodymyr Gladun <vgladun@gmail.com>
2020-03-30 00:57:58 +0100

    added AppVeyor support

--- test/lib_Tests.cpp
+++ test/lib_Tests.cpp
@@ -9,9 +9,10 @@ TEST_CASE("testing my library", "[lib]") {
   auto l = lib{};
 
   SECTION("testing addition") {
-    REQUIRE(l.addition(1, 41) == 42);
-    REQUIRE(l.addition(std::numeric_limits<int32_t>::max(),
-                       std::numeric_limits<int32_t>::max()) == 4294967294);
+    auto sum = l.addition(1, 41);
+    REQUIRE(sum == 42);
+    sum = l.addition(std::numeric_limits<int32_t>::max(), std::numeric_limits<int32_t>::max());
+    REQUIRE(sum == 4294967294);
   }
 
   SECTION("testing throw") {
