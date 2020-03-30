469bd280ef007d87fd4178cd9b154a09327d3c78
Volodymyr Gladun <vgladun@gmail.com>
2020-03-30 03:10:57 +0100

    appveyor changes

--- include/detail/Precompiled.hpp
+++ include/detail/Precompiled.hpp
@@ -1,11 +1,11 @@
 #ifndef PROJECT_INCLUDE_DETAIL_PRECOMPILED_HPP
 #define PROJECT_INCLUDE_DETAIL_PRECOMPILED_HPP
 
-#ifdef _WIN32
-#  define WIN32_LEAN_AND_MEAN
-#elif __linux__
-#else
-#endif
+//#ifdef _WIN32
+//#  define WIN32_LEAN_AND_MEAN
+//#elif __linux__
+//#else
+//#endif
 
 #include "ErrHandler.hpp"
 #include "StringUtils.hpp"
