b763714b1d08abd86149ebd8dffc77ae94dd30d8
Volodymyr Gladun <vgladun@gmail.com>
2020-04-23 01:59:17 +0100

    refactored ErrHandler.hpp and StringUtils.hpp

--- include/detail/ErrHandler.hpp
+++ include/detail/ErrHandler.hpp
@@ -7,27 +7,19 @@
 
 namespace detail {
 
-  static void print_nested_exception(
-      const std::exception_ptr & eptr = std::current_exception(),
-      size_t level = 0) {
-    static auto get_nested = [](auto & e) -> std::exception_ptr {
-      try {
-        return dynamic_cast<const std::nested_exception &>(e).nested_ptr();
-      }
-      catch (...) {
-        return nullptr;
-      }
-    };
-
+  static void print_nested_exception(const std::exception& e, size_t level =  0) {
+    std::cerr << std::string(level, ' ') << "exception: " << e.what() << '\n';
     try {
-      if (eptr) { std::rethrow_exception(eptr); }
-    }
-    catch (const std::exception & e) {
-      std::cerr << std::string(level * 2, ' ')
-                << "exception: " << e.what() << '\n';
-      // rewind all nested exception
-      print_nested_exception(get_nested(e), level + 1);
-    }
+      // Check if exception has nested exception
+      // If not means that it is last level and we simply return
+      try {
+        if(!dynamic_cast<const std::nested_exception &>(e).nested_ptr()) { return; }
+      } catch (...) { return; }
+
+      std::rethrow_if_nested(e);
+    } catch(const std::exception& e) {
+      print_nested_exception(e, level + 2);
+    } catch(...) {}
   }
 
 }
--- include/detail/StringUtils.hpp
+++ include/detail/StringUtils.hpp
@@ -1,17 +1,16 @@
 #ifndef PROJECT_INCLUDE_DETAIL_UTILS_HPP
 #define PROJECT_INCLUDE_DETAIL_UTILS_HPP
 
-#include <iostream>
 #include <sstream>
 
 namespace detail {
 
   template<typename... T>
-    static std::string concat (T const & ... args) {
-      std::ostringstream os{};
-      (os << ... << args);
-      return os.str();
-    }
+  static std::string concat (T const & ... args) {
+    std::ostringstream os{};
+    (os << ... << args);
+    return os.str();
+  }
 
 }
 
